6ef3b93c1e77bc85d13a4389963972fb48020dac
Evren Ermiş <36535914+eermis1@users.noreply.github.com>
2020-05-02 18:42:40 +0300

    DB - Setting Update

--- cityapp/cityapp/settings.py
+++ cityapp/cityapp/settings.py
@@ -95,13 +95,12 @@ WSGI_APPLICATION = 'cityapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 # DATABASES = {
 #    'default': {
@@ -115,16 +114,16 @@ WSGI_APPLICATION = 'cityapp.wsgi.application'
 #    }
 #}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'swe574_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Seneevo1992',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'swe574_db',
+#        'USER': 'postgres',
+#        'PASSWORD': 'Seneevo1992',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 # DATABASES = {
 #     'default': {
