66f0b2cb6c322a0193457443d50b1db6c676b74c
jrqc <jrqc01@hotmail.com>
2020-05-18 12:24:23 +0300

    build test

--- cityapp/city/tests.py
+++ cityapp/city/tests.py
@@ -1,9 +1,3 @@
 from django.test import TestCase
-import json
-
-from community.models import *
-from community_user.models import CommunityUser
-from django.urls import reverse
-from rest_framework.test import APITestCase
 
 # Create your tests here.
