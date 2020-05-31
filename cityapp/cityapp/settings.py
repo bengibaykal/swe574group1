108ba716de2da030a2e2201c444e9f23f75a78c7
jrqc <jrqc01@hotmail.com>
2020-05-31 22:24:19 +0300

    init

--- cityapp/cityapp/settings.py
+++ cityapp/cityapp/settings.py
@@ -96,12 +96,12 @@ WSGI_APPLICATION = 'cityapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -126,17 +126,17 @@ DATABASES = {
 #     }
 # }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'swe'
-#                '573',
-#        'USER': 'gurbuz',
-#        'PASSWORD': '',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'swe'
+               '573',
+       'USER': 'gurbuz',
+       'PASSWORD': '',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
+}
 
 # Activity Stream Settings
 ACTSTREAM_SETTINGS = {
--- cityapp/community/models.py
+++ cityapp/community/models.py
@@ -59,9 +59,9 @@ class Community(TimeStamped):
     public_option = models.BooleanField(default=True)
     city_id = models.ForeignKey(City, related_name="city_id", on_delete=models.CASCADE, blank=True, null=True)
 
-
     def __str__(self):
         return smart_unicode(self.name)
+
 
 # Creating Action Instances by Using Django Signals & Actstream Action
 def save_community(sender, instance, **kwargs):
@@ -105,6 +105,7 @@ class PostTemplate(TimeStamped):
     def __str__(self):
         return str(self.name)
 
+
 # Creating Action Instances by Using Django Signals & Actstream Action
 def save_posttemplate(sender, instance, **kwargs):
     action.send(instance.created_by, verb="Has Created A New Post Template - ", description="New Post Template",
@@ -112,6 +113,7 @@ def save_posttemplate(sender, instance, **kwargs):
 
 
 post_save.connect(save_posttemplate, sender=PostTemplate)
+
 
 class Post(TimeStamped):
     name = models.CharField(max_length=55, blank=True, null=True)
@@ -123,14 +125,15 @@ class Post(TimeStamped):
     post_template = models.ForeignKey(PostTemplate, related_name="post_template", on_delete=models.CASCADE, blank=True,
                                       null=True)
     post_content = JSONField(null=True)
-    latitude = models.FloatField(null=True, blank=True) # To Fix Post Creation Issue Due To Empty Lat
-    longitude = models.FloatField(null=True, blank=True) # To Fix Post Creation Issue Due To Empty Long
+    latitude = models.FloatField(null=True, blank=True)  # To Fix Post Creation Issue Due To Empty Lat
+    longitude = models.FloatField(null=True, blank=True)  # To Fix Post Creation Issue Due To Empty Long
     audio_version = models.FileField(max_length=55, null=True)
-    flags = models.IntegerField(default=0, null=True, blank=True,)
-    flaggedUsers = models.ManyToManyField(CommunityUser, related_name= 'flagged_users')
+    flags = models.IntegerField(default=0, null=True, blank=True, )
+    flaggedUsers = models.ManyToManyField(CommunityUser, related_name='flagged_users')
 
     def __str__(self):
         return smart_unicode(self.name)
+
 
 # Creating Action Instances by Using Django Signals & Actstream Action
 def save_post(sender, instance, **kwargs):
@@ -140,6 +143,7 @@ def save_post(sender, instance, **kwargs):
 
 post_save.connect(save_post, sender=Post)
 
+
 class DataFileField(TimeStamped):
     name = models.CharField(max_length=55)
     file_field = models.FileField(max_length=55, null=True)
@@ -148,14 +152,16 @@ class DataFileField(TimeStamped):
     def __str__(self):
         return smart_unicode(self.name)
 
+
 class Comment(TimeStamped):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="related_post")
     created_by = models.ForeignKey(CommunityUser, on_delete=models.CASCADE, related_name="comment_author",
-                                        null=True)
+                                   null=True)
     content = models.TextField(null=True)
 
     def __str__(self):
         return self.post.name
+
 
 # Creating Action Instances by Using Django Signals & Actstream Action
 def save_comment(sender, instance, **kwargs):
@@ -164,6 +170,7 @@ def save_comment(sender, instance, **kwargs):
 
 
 post_save.connect(save_comment, sender=Comment)
+
 
 # todo
 class Recommendation(models.Model):
