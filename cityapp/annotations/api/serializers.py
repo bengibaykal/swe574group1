7fd6a67a402c511adff2bf698e706c7778f581d5
jrqc <jrqc01@hotmail.com>
2020-06-02 22:34:59 +0300

    annotations migrations created

--- cityapp/annotations/api/serializers.py
+++ cityapp/annotations/api/serializers.py
@@ -2,41 +2,33 @@ from rest_framework import serializers
 from annotations.models import Annotation
 
 
-
-
 class AnnotationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Annotation
-        fields = [ 'annotation' ]
-        validators = [ ]
+        fields = ['annotation']
+        validators = []
 
 
 schema = {
-    "type" : "object",
-     "properties" : {
-        "price" : {"type" : "number"},
-         "name" : {"type" : "string"},
-     },
- }
+    "type": "object",
+    "properties": {
+        "price": {"type": "number"},
+        "name": {"type": "string"},
+    },
+}
 
 
 class AnnotationCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Annotation
-        fields = [ 'annotation' ]
+        fields = ['annotation']
 
-    #def validate_annotation(self, value):
-        #if value = "ben":
-        #    raise serializers.ValidationError("Input data is not in a valid W3C Annotation Data Model.")
-        #return value
-    #def validate(self, attrs):
-     #   if attrs['annotation'] != '':
-     #       raise serializers.ValidationError("The schema is null")
-     #   validate(instance=self, schema=schema)
-     #   return attrs
-
-
-
-
-
-
+    # def validate_annotation(self, value):
+    # if value = "ben":
+    #    raise serializers.ValidationError("Input data is not in a valid W3C Annotation Data Model.")
+    # return value
+    # def validate(self, attrs):
+    #   if attrs['annotation'] != '':
+    #       raise serializers.ValidationError("The schema is null")
+    #   validate(instance=self, schema=schema)
+    #   return attrs
--- cityapp/annotations/api/views.py
+++ cityapp/annotations/api/views.py
@@ -1,28 +1,33 @@
-from rest_framework.generics import (ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView, CreateAPIView )
+from rest_framework.generics import (ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView, CreateAPIView)
 from annotations.models import Annotation
 from annotations.api.serializers import AnnotationSerializer, AnnotationCreateSerializer
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 class AnnotationAPIView(ListAPIView):
     queryset = Annotation.objects.all()
     serializer_class = AnnotationSerializer
+
 
 class AnnotationDetailAPIView(RetrieveAPIView):
     queryset = Annotation.objects.all()
     serializer_class = AnnotationSerializer
     lookup_field = 'pk'
 
+
 class AnnotationDeleteAPIView(DestroyAPIView):
     queryset = Annotation.objects.all()
     serializer_class = AnnotationSerializer
     lookup_field = 'pk'
 
+
 class AnnotationUpdateAPIView(UpdateAPIView):
     queryset = Annotation.objects.all()
     serializer_class = AnnotationSerializer
     lookup_field = 'pk'
+
 
 class AnnotationCreateAPIView(CreateAPIView):
     queryset = Annotation.objects.all()
@@ -31,11 +36,5 @@ class AnnotationCreateAPIView(CreateAPIView):
     @api_view(['GET', 'POST'])
     def hello_world(request):
         if request.method == 'POST':
-
             return Response({"message": "Got some data!", "data": request.data})
         return Response({"message": "Hello, world!"})
-
-
-
-
-
